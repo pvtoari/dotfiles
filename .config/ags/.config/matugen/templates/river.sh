de5e85e2e3c8beef0921de004c6fdbe857274f03
pvtoari <arielroque@protonmail.com>
2025-11-19 17:05:43 +0100

    Remove AGS upper bar stuff

--- .config/ags/.config/matugen/templates/river.sh
+++
@@ -1,4 +0,0 @@
-riverctl background-color 0x{{colors.background.default.hex_stripped}}
-riverctl border-color-focused 0x{{colors.primary.default.hex_stripped}}
-riverctl border-color-unfocused 0x{{colors.background.default.hex_stripped}}
-
--- .config/ags/.zshenv
+++
@@ -1,6 +0,0 @@
-ZDOTDIR=$HOME/.config/zsh
-
-export STARSHIP_CONFIG=$ZDOTDIR/starship.toml
-export PATH="$PATH:~/.local/bin:~/.ghcup/bin"
-export TERMINAL=foot
-
